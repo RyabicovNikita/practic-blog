18522fc6303909300c94e98876f0a7f00230c6e4
RyabicovNikita <RyabicovN1k@yandex.ru>
2024-11-16 17:05:45 +0300

    connect Users component

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes, useLocation } from "react-router";
 import "./Blog.css";
-import { Main, Header } from "./components";
-import { Footer } from "./components/Footer/Footer";
+import { Main, Header, Footer } from "./components";
 import { AuthForm } from "./pages/AuthForm/AuthForm";
+import { Users } from "./pages/Users/Users";
 
 function Blog() {
   const location = useLocation();
@@ -13,7 +13,7 @@ function Blog() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/auth" element={<AuthForm>SIGN IN</AuthForm>} />
-        <Route path="/users" element={<div>Пользователи</div>} />
+        <Route path="/users" element={<Users />} />
         <Route path="/post" element={<div>Новая статья</div>} />
         <Route path="/post/:postId" element={<div>Статья</div>} />
         <Route path="*" element={<div>Ошибка</div>} />
