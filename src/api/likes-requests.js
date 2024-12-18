a0337fded245a195850a3c15ad72889d340e0625
RyabicovNikita <RyabicovN1k@yandex.ru>
2024-12-18 12:09:24 +0300

    fix comments

--- src/api/likes-requests.js
+++ src/api/likes-requests.js
@@ -3,6 +3,11 @@ export const fetchGetLikes = () =>
     .then((res) => res.json())
     .then((likes) => likes);
 
+export const fetchGetComments = () =>
+  fetch("http://localhost:3005/comments")
+    .then((res) => res.json())
+    .then((comments) => comments);
+
 export const fetchGetLike = (user_id, post_id) =>
   fetch(`http://localhost:3005/likes?user_id=${user_id}&post_id=${post_id}`).then((res) => res.json());
 
